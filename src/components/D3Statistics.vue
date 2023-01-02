4baf00cbfe5bf9f73ccc298bbfcaa53d713c6233
Kletsyn <k.kletsyn@kryptonite.ru>
2023-01-03 01:39:04 +0300

    Add tooltip

--- src/components/D3Statistics.vue
+++ src/components/D3Statistics.vue
@@ -8,6 +8,7 @@
         <button @click="onButtonClick(365 * 2)">2 года</button>
         <button @click="showAll">За всё время</button>
         <svg></svg>
+        <div class="tooltip"></div>
     </div>
 </template>
 <script lang='ts'>
@@ -26,12 +27,12 @@ interface StatisticsDatum {
 }
 
 interface ComponentData {
-    svg?: SVGSVGElement,
     svgSelection?: D3Selection,
     barChart?: D3Selection,
     xAxis?: D3Selection,
     yAxis?: D3Selection,
     slider?: D3Selection,
+    tooltip?: D3Selection,
     data: StatisticsDatum[],
     margin: {
         left: number,
@@ -45,11 +46,11 @@ interface ComponentData {
 }
 
 type D3Selection = d3.Selection<any, any, any, any>;
-const PARSED_DATA = dataJson.map((elem: StatisticsDatum) => ({ ...elem, date: new Date(elem.date) })) as StatisticsDatum[]
+
+const PARSED_DATA = dataJson.map((elem: StatisticsDatum) => ({ ...elem, date: new Date(elem.date) })) as StatisticsDatum[];
 const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
 const MONTH_IN_MILLISECONDS = DAY_IN_MILLISECONDS * 31;
 const MAXIMUM_BAR_WIDTH = 80;
-const DEFAULT_TICKS_COUNT = 4;
 
 export default defineComponent({
     name: 'D3Statistics',
@@ -60,6 +61,7 @@ export default defineComponent({
             xAxis: undefined,
             yAxis: undefined,
             slider: undefined,
+            tooltip: undefined,
             data: [],
             margin: {
                 left: 30,
@@ -89,7 +91,7 @@ export default defineComponent({
             this.renderAxises();
             this.renderBars();
             this.renderSlider();
-            this.svg = this.svgSelection.node();
+            this.initTooltip();
         },
         renderAxises(): void {
             this.yAxis
@@ -99,7 +101,7 @@ export default defineComponent({
                     g.selectAll('.tick .copy').remove();
                     g.selectAll('.tick line').clone()
                         .attr('stroke-opacity', 0.15)
-                        .attr('x2', this.width)
+                        .attr('x2', this.width + this.xBandwidth / 2)
                         .classed('copy', d => d !== 1);
                 });
             this.xAxis?.attr('transform', `translate(${this.margin.left + this.xBandwidth / 2},${this.height})`);
@@ -116,7 +118,7 @@ export default defineComponent({
                     sameElse: 'HH:mm',
                 });
             }));
-            this.xAxis?.select('.domain').attr('d', `M${0 - this.xBandwidth / 2},0H${this.width - this.xBandwidth / 2}`);
+            this.xAxis?.select('.domain').attr('d', `M${0 - this.xBandwidth / 2},0H${this.width + this.xBandwidth / 2}`);
         },
         renderBars(): void {
             const t = d3.transition().duration(750).ease(d3.easePoly);
@@ -127,16 +129,37 @@ export default defineComponent({
                     update => update,
                     exit => exit.call(rect => rect.transition(t).remove().attr('y', this.height).attr('height', 0)),
                 )
-                .call(text => text.transition(t)
+                .call(rect => rect.transition(t)
                     .attr('width', this.xBandwidth)
                     .attr('height', data => this.height - this.y(data.upper))
                     .attr('x', (elem) => this.x(elem.date))
                     .attr('y', (datum) => this.y(datum.upper))
-                    .attr('fill', '#6c8191'));
+                    .attr('fill', '#6c8191')
+                );
+            this.barChart?.selectAll('.bar')
+                .on('mouseover', this.mouseoverBar)
+                .on('mousemove', this.mousemoveBar)
+                .on('mouseleave', this.mouseleaveBar);
             this.barChart?.transition(t).attr('transform', `translate(${this.margin.left}, 0)`);
         },
         renderSlider(): void {
             // todo
+        },
+        initTooltip(): void {
+            this.tooltip = d3.select('.tooltip');
+            this.tooltip.classed('tooltip', true);
+        },
+        mouseoverBar(g: any): void {
+            this.tooltip?.style('opacity', 1);
+        },
+        mousemoveBar(event: MouseEvent): void {
+            const data: StatisticsDatum = (event.target as any).__data__;
+            this.tooltip?.html(`<div>${moment(data.date).format('DD.MM.YYYY HH:mm:ss')}<br><span class='tooltip-value'>${data.upper.toFixed()}</span> записей</div>`)
+                .style('left', (d3.pointer(event, event.target)[0]) - 11 + 'px')
+                .style('top', (d3.pointer(event, event.target)[1] - 25) + 'px')
+        },
+        mouseleaveBar(g: any): void {
+            this.tooltip?.style('opacity', 0)
         },
         onButtonClick(days: number): void {
             // filter values between selected date and current date
@@ -175,20 +198,21 @@ export default defineComponent({
                 prev = elem;
                 return acc;
             }, 160);
-            return d3.min([minimalDiff / 2, MAXIMUM_BAR_WIDTH]) ?? MAXIMUM_BAR_WIDTH;
+            const diffFactor = d3.median([this.data.length / PARSED_DATA.length, 1]) ?? 0.5; // coefficient for convenient bar width
+            return d3.min([minimalDiff * diffFactor, MAXIMUM_BAR_WIDTH]) ?? MAXIMUM_BAR_WIDTH;
         },
         y(): d3.ScaleLinear<number, number, never> {
             return d3.scaleLinear()
                 .range([this.height, this.margin.top]).nice()
-                .domain([0, d3.max(this.data, datum => datum.upper) ?? 1]);
+                .domain([0, d3.max(this.data, datum => datum.upper) ?? 10]);
         },
         ticksCount(): number {
-            return d3.min([this.width / 80, this.data.length]) || DEFAULT_TICKS_COUNT;
+            return this.width / 120;
         },
     }
 });
 </script>
-<style>
+<style lang="scss">
 .statistics {
     background-color: #f5f6f7;
     width: min-content;
@@ -196,5 +220,24 @@ export default defineComponent({
 }
 .bar {
     cursor: pointer;
+    &:hover {
+        fill: #4fa292;
+    }
+}
+.tooltip {
+    position: absolute;
+    font-family: 'Roboto';
+    font-size: 12px;
+    font-weight: 500;
+    text-align: center;
+    padding: 12px;
+    background-color: #e8eaed;
+    border: 1px solid #aeb6be;
+    border-radius: 5px;
+    opacity: 0;
+    &-value {
+        color: #4fa292;
+        font-weight: bold;
+    }
 }
 </style>
